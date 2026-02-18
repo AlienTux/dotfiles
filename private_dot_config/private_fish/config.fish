cdec6459ab1d0b8de9c3577dd00e170581830a31
AlienTux <kellner.kurt@pm.me>
2026-02-17 19:06:24 -0600

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -9,3 +9,12 @@ abbr dup 'sudo zypperoni ref && sudo zypper dup'
 abbr h 'hledger -f ~/pCloud_local/PTA/daily.journal'
 abbr pta 'cd ~/pCloud_local/PTA/'
 abbr nnn 'nnn -P p -d'
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
